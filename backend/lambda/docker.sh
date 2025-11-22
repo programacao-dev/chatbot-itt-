6781dc660d22775d9df8084cab8d4ccb4a299788
rbfcdog <rodrigobfcdog@gmail.com>
2025-11-21 23:27:21 -0300

    docker

--- backend/lambda/docker.sh
+++
@@ -1,5 +0,0 @@
-curl -X POST "http://localhost:9000/2015-03-31/functions/function/invocations" \
-     -H "Content-Type: application/json" \
-     -d '{
-          "query": "como serao assinados os cheques do ITT"
-         }'
